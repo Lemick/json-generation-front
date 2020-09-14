586d2263658fc035a1218d74c58b0937ee46a8d0
Mickael B <sidfloyd84@gmail.com>
2020-09-14 22:48:03 +0200

    Init

---
+++ src/app/pages/json-edit/json-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-json-edit',
+  templateUrl: './json-edit.component.html',
+  styleUrls: ['./json-edit.component.css']
+})
+export class JsonEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
